79cd123a5631facafd8ac7611c8bb6a6c0289ae2
Ramirez-Facu <98377628+Ramirez-Facu@users.noreply.github.com>
2025-01-31 12:57:15 -0300

    Added presence of element for better assertion

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,7 @@
 from pages.login_page import LoginPage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def test_login_fail(driver):
     login_page = LoginPage(driver)
@@ -13,3 +14,6 @@ def test_login_success(driver):
     
     WebDriverWait(driver, 10).until(EC.url_contains("inventory.html"))
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "title")))
+    assert driver.find_element(By.CLASS_NAME, "title").text == "Products"
