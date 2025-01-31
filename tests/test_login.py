1a52e07eec504904fd6d30d77c485bf2fce422e7
Ramirez-Facu <98377628+Ramirez-Facu@users.noreply.github.com>
2025-01-31 12:44:36 -0300

    Added login success test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,15 @@
 from pages.login_page import LoginPage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_login_fail(driver):
     login_page = LoginPage(driver)
     login_page.login("user", "password")
 
     assert login_page.get_error_message() == "Epic sadface: Username and password do not match any user in this service"
+def test_login_success(driver):
+    login_page = LoginPage(driver)
+    login_page.login("standard_user","secret_sauce")
     
+    WebDriverWait(driver, 10).until(EC.url_contains("inventory.html"))
+    assert driver.current_url == "https://www.saucedemo.com/inventory.html"
